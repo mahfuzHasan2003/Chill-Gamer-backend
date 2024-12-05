10dffe685e4fccbfb1e6a60dd9ec4cd3da64c641
mahfuzHasan2003 <mahfuzhasan2003@yahoo.com>
2024-12-05 21:43:07 +0600

    added watchlist, myreviews etc

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri =
    "mongodb+srv://mahfuz:uH5ZpdgMjhrcobUK@cluster0.xggde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -23,6 +23,7 @@ async function run() {
       console.log("Successfully connected");
       const database = client.db("chillGamerDB");
       const reviewsCollection = database.collection("reviewsCollection");
+      const watchListCollection = database.collection("watchListCollection");
       app.get("/", (req, res) => {
          res.send("Hey mutter");
       });
@@ -30,9 +31,36 @@ async function run() {
          const result = (await reviewsCollection.find().toArray()) || [];
          res.send(result);
       });
+      app.get("/my-reviews/user/:user", async (req, res) => {
+         const userMail = req.params.user;
+         const query = { userEmail: userMail };
+         const result = (await reviewsCollection.find(query).toArray()) || [];
+         res.send(result);
+      });
       app.post("/reviews", async (req, res) => {
          const newReview = req.body;
          const result = await reviewsCollection.insertOne(newReview);
+         res.send(result);
+      });
+      app.get("/review/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) };
+         const result = await reviewsCollection.findOne(query);
+         console.log(res.send(result));
+      });
+      // app.get("/watchList", async (req, res) => {
+      //    const result = (await watchListCollection.find().toArray()) || [];
+      //    res.send(result);
+      // });
+      app.get("/watchList/:user", async (req, res) => {
+         const userMail = req.params.user;
+         const query = { userEmail: userMail };
+         const result = (await watchListCollection.find(query).toArray()) || [];
+         res.send(result);
+      });
+      app.post("/watchList", async (req, res) => {
+         const newReview = req.body;
+         const result = await watchListCollection.insertOne(newReview);
          res.send(result);
       });
    } catch (error) {
